daaa531ce6c44166f75a50f209d807b3a56ecc3e
Jaine Beling <jaine.mazuim2016@gmail.com>
2023-08-17 20:46:31 -0300

    strings

---
+++ tipos-de-dados/exercicios.js
@@ -0,0 +1,28 @@
+//Declare uma variável contendo uma string
+var estado = "Rio Grande do Sul";
+console.log(estado);
+console.log(typeof estado);
+
+//Declare uma variável contendo um número dentro de uma string
+var nascimento = "1999";
+console.log(nascimento);
+console.log(typeof nascimento);
+
+//Declare uma variável com a sua idade
+var idade = 23;
+console.log(idade);
+
+//Declare duas variáveis, uma com seu nome e outra com seu sobrenome e some as mesmas
+var nome = "Jaine";
+var sobrenome = "Beling";
+console.log(nome + " " + sobrenome);
+
+//Coloque a seguinte frase em uma variável: It's time
+var frase1 = "It's time";
+console.log(frase1);
+
+//Verifique o tipo da variável que contém o seu nome
+var nome = "Jaine";
+var sobrenome = "Beling";
+var nomeCompleto = nome + " " + sobrenome;
+console.log(typeof nomeCompleto);
